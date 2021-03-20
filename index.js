3c5af3d25519fbe04b8e88cbacc95c83ffa6ffc9
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-03-20 14:51:31 -0400

    node part

---
+++ index.js
@@ -0,0 +1,62 @@
+const fs = require("fs")
+const csv = require("csvtojson")
+
+const dictionaryHeaderFile = "./header.txt"
+const vocabFile = "./vocabulary.csv"
+
+const dictionaryHeader = fs.readFileSync(dictionaryHeaderFile, "utf-8").toString()
+
+const villagerMode = process.argv[2]?.toLowerCase().startsWith("v")
+
+csv()
+  .fromFile(vocabFile)
+  .then(async words => {
+    let dictionaryText = ""
+
+    for(const entry of words) {
+      let entryLines = ["\n"]
+
+      const { word, partOfSpeech, translation, example, notes, v_translation } = entry
+
+      const fnl = str => str.replace(/\|/g, "\n") // fix new lines function
+      const numberize = (str, useLetters, useAsteriskBullets) => {
+        const lines = str.split("\n")
+        const numberedLines = []
+        for(let i in lines) {
+          let number = (parseInt(i) + 1).toString() + "."
+          if(useLetters) {
+            const letters = "abcdefghijklmnopqrstuvwxyz"
+            number = letters.charAt(i % letters.length) + "."
+          }
+          if(useAsteriskBullets) {
+            number = "*"
+          }
+
+          numberedLines.push(`${number} ${lines[i]}`)
+        }
+
+        return numberedLines.join("\n")
+      }
+
+
+      entryLines.push(`§0§l${word} §0§o${partOfSpeech}`)
+      if(villagerMode) {
+        entryLines.push(`§0${numberize(fnl(v_translation))}`)
+        entryLines.push(`§7${numberize(fnl(translation))}`)
+      }
+      else {
+        entryLines.push(`§0${numberize(fnl(translation))}`)
+        example && entryLines.push(`§7${numberize(fnl(example), true)}`)
+        notes   && entryLines.push(`§7§o${numberize(fnl(notes), null, true)}`)
+      }
+
+      dictionaryText += entryLines.join("\n")
+    }
+
+    const fileName = `./output/${Date.now()}.txt`
+    fs.writeFile(fileName, dictionaryHeader + dictionaryText, err => {
+      if(err)
+        return console.log(err)
+      console.log(`Written to ${fileName}!`)
+    })
+  })
