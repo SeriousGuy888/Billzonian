a201ec835a703b6a9a20f31ddf6d500726924a9d
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-11-12 15:32:05 -0500

    implement arrangement questions with petite vue

--- docs/assets/exercises/components.js
+++ docs/assets/exercises/components.js
@@ -7,11 +7,13 @@ function MainPanel() {
     chosenTopicData: null,
     questionNumber: 0,
     answerHistory: {
+      currentCorrect: false,
       correct: 0,
       total: 0,
     },
     answersRevealed: false,
     selectedAnswer: null,
+    selectedArrangement: [],
     topics: {
       present_tense: {
         title: "Present Tense",
@@ -25,10 +27,9 @@ function MainPanel() {
     },
     async chooseTopic(id) {
       this.questionNumber = 0
+      this.answerHistory.currentCorrect = false
       this.answerHistory.correct = 0
       this.answerHistory.total = 0
-      this.answersRevealed = false
-      this.selectedAnswer = null
 
       if(!id) {
         this.chosenTopic = null
@@ -42,38 +43,74 @@ function MainPanel() {
       
       this.chosenTopic = id
       this.chosenTopicData = await res.json()
+
+      this.initNextQuestion(false) 
     },
     revealAnswers() {
       if(!this.answersRevealed) {
         this.answersRevealed = true
 
         const currentQuestion = this.chosenTopicData[this.questionNumber]
+
         if(currentQuestion.type === "multiple_choice") {
           if(currentQuestion.answers[this.selectedAnswer]?.akrat) {
             this.answerHistory.correct++
           }
         }
+        if(currentQuestion.type === "arrange") {
+          const replacementRegex = /[^a-z0-9 ]/g
+          const correctAnswers = currentQuestion.akrats
+            .map(e => e
+              .toLowerCase()
+              .replace(replacementRegex, "")
+            )
+
+          const words = this.selectedArrangement.map(e => currentQuestion.blocks[e])
+          const answerStr = words.join(" ")
+            .toLowerCase()
+            .replace(replacementRegex, "")
+          
+          if(correctAnswers.includes(answerStr)) {
+            this.answerHistory.correct++
+            this.answerHistory.currentCorrect = true
+          }
+        }
 
         this.answerHistory.total++
       } else {
-        this.answersRevealed = false
-        this.questionNumber++
-        this.selectedAnswer = null
+        this.initNextQuestion(true)
+      }
+    },
+    initNextQuestion(incrementQuestionNumber) {
+      this.answersRevealed = false
+      this.selectedAnswer = null
+      this.selectedArrangement = []
+      this.answerHistory.currentCorrect = false
 
-        const currentQuestion = this.chosenTopicData[this.questionNumber]
-        if(currentQuestion.type === "multiple_choice") {
-          currentQuestion.answers = this.shuffle(currentQuestion.answers)
-        }
+      if(incrementQuestionNumber)
+        this.questionNumber++
+
+      const currentQuestion = this.chosenTopicData[this.questionNumber]
+      if(currentQuestion.type === "multiple_choice") {
+        currentQuestion.answers = this.shuffle(currentQuestion.answers)
+      }
+      if(currentQuestion.type === "arrange") {
+        currentQuestion.blocks = this.shuffle(currentQuestion.blocks)
+      }
+    },
+    updateArrangement(index, removing) { // removing means removing from the array of already placed blocks
+      if(this.answersRevealed)
+        return
+
+      if(removing) {
+        this.selectedArrangement.splice(index, 1)
+      } else {
+        if(!this.selectedArrangement.includes(index))
+          this.selectedArrangement.push(index)
       }
     },
     shuffle(arr) {
-      let newArr = []
-      for(let i = 0; i < arr.length; i++) {
-        let elem = arr[i]
-        elem.originalIndex = i
-        newArr.push(elem)
-      }
-
+      let newArr = arr.map(e => e) // duplicate array & prevent mutation
       return newArr.sort(() => Math.random() - 0.5) // return shuffled
     },
   }
